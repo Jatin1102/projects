5cb8ab218a5fdc0501509f43926be4f4c8050dda
Jatin <jatin@LAPTOP-5GP1NRVR>
2020-10-05 21:44:01 +0530

    m

--- new/phone.js
+++ new/phone.js
@@ -1,3 +1,5 @@
+var boolButton = false;
+
 window.onload = function () {
   render();
 };
@@ -31,6 +33,7 @@ function codeverify() {
   coderesult
     .confirm(code)
     .then(function (result) {
+      boolButton = true;
       alert("Successfully registered");
       var user = result.user;
       console.log(user);
@@ -39,3 +42,36 @@ function codeverify() {
       alert(error.message);
     });
 }
+
+//Signup.js
+const database = firebase.database();
+const rootRef = database.ref("users");
+
+const name = document.getElementById("inputName");
+const email = document.getElementById("inputEmail");
+const password = document.getElementById("inputPassword");
+const phoneNumber = document.getElementById("inputPhone");
+const addBtn = document.getElementById("quickstart-sign-in");
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  // const autoId = rootRef.push().key;
+  rootRef.child(phoneNumber.value).set({
+    Name: name.value,
+    Email: email.value,
+    Password: password.value,
+    PhoneNumber: phoneNumber.value,
+  });
+});
+
+$("#verifyButton").on("click", function () {
+  if (boolButton) {
+    $(".form-signin input:required").each(function () {
+      if (this.value === "") {
+        alert("Please fill all field");
+      } else {
+        addBtn.disabled = false;
+      }
+    });
+  }
+});
--- new/signup.js
+++ new/signup.js
@@ -9,11 +9,21 @@ const addBtn = document.getElementById("quickstart-sign-in");
 
 addBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  const autoId = rootRef.push().key;
-  rootRef.child(autoId).set({
+  // const autoId = rootRef.push().key;
+  rootRef.child(phoneNumber.value).set({
     Name: name.value,
     Email: email.value,
     Password: password.value,
     PhoneNumber: phoneNumber.value,
+  });
+});
+
+$(addBtn).on("click", function () {
+  $(".form-signin input:required").each(function () {
+    if (this.value === "") {
+      alert("Please fill all field");
+    } else {
+      addBtn.disabled = false;
+    }
   });
 });
